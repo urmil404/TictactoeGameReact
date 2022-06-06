8a1035545e504fcc05a4f6d892ae1c92923c8065
urmil404 <urmil8989@gmail.com>
2022-06-06 18:35:16 +0530

    send icons to components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Icon from "./components/icons";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setwinMessage] = useState("");
+
+  const reloadGame = () => {};
+  const checkIsWinner = () => {};
+
+  const changeItem = () => {};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Icon />
       </header>
     </div>
   );
-}
+};
 
 export default App;
